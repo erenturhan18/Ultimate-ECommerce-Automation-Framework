34eeb031671f888e2e14b9c7b0802fdb23358df4
erenturhan18 <erenturhan120@gmail.com>
2026-02-09 01:16:01 +0300

    Refactor: implemented BDD structure and Hamcrest matchers for API tests

--- src/test/java/apiTests/C01_GetRequest.java
+++ src/test/java/apiTests/C01_GetRequest.java
@@ -4,23 +4,28 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 public class C01_GetRequest {
 
     @Test
-    public void test01(){
+    public void test01() {
         String url = "https://jsonplaceholder.typicode.com/posts/2";
-        Response response = RestAssured.get(url);
+        given()
+                .baseUri(url)
+                .header("Content-Type","applicatio/json")
+                .when()
+                    .get()
+                .then()
+                    .assertThat()
+                    .statusCode(200)
+                    .contentType("application/json")
+                    .body("title",equalTo("qui est esse"))
+                    .body("userId",equalTo(1))
+                    .log().all();
+        System.out.println("Profesyonel BDD doğrulamaları başarıyla tamamlandı!");
 
-        System.out.println("Status code: " + response.getStatusCode());
-        System.out.println("Content Type: " + response.getContentType());
-
-        response.prettyPrint();
-        Assert.assertEquals(response.getStatusCode(), 200, "Status code 200 olmalıydı");
-
-        Assert.assertEquals(response.path("title"), "qui est esse");
-        Assert.assertEquals(response.path("userId").toString(), "1");
-        System.out.println("Doğrulamalar başarıyla tamamlandı!");
 
 
 
